d853e9db2f9f5b3df922a5264effe176d01bdb58
ziyintonghua <lingfeng19900808@163.com>
2022-10-30 08:27:00 +0000

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+    char a='e';
+    switch (a){
+        case 'a':
+        printf("我爱你胡婷婷\n");
+        break;
+        case 'b':
+        printf("我爱你甘婷婷\n");
+        break;
+        case 'c':
+        printf("我爱你李依晓\n");
+        break;
+        case 'd':
+        printf("我爱你殷桃\n");
+        break;
+        case 'e':
+        printf("我爱你孟璐\n");
+        break;
+        case 'f':
+        printf("我爱你郭珍妮\n");
+        break;
+        case 'g':
+        printf("我爱你冯琳\n");
+        break;
+    }    
+}
