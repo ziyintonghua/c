9e8a85ac54e3bd612065e2d6ccaa9d91f004f326
ziyintonghua <lingfeng19900808@163.com>
2022-10-30 08:35:07 +0000

    Create goto.c

---
+++ goto.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc,char *argv[]){
+    printf("我爱胡婷婷\n");
+    goto xiutao;
+    printf("我爱李依晓\n");
+    printf(”我爱孟璐\n");
+xiutao:
+    printf("我爱甘婷婷\n");
+    printf("我爱佟丽娅\n");
+    return 0;
+}
